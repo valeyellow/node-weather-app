11d44fb3c366b8279259d573b56b71c846cbba25
valeyellow <sumit.mishtra.4693@gmail.com>
2021-07-08 23:58:41 +0530

    Added templates and routes for home, about and help

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Server, Request, Response } from 'express';
 import * as dotenv from 'dotenv';
 import path from 'path';
+import hbs from 'hbs';
 
 dotenv.config();
 
@@ -9,14 +10,32 @@ if(!process.env.PORT) {
 }
 const PORT: number = parseInt(process.env.PORT as string, 10);
 
-const app = express();
+const app: Server = express();
 
 const publicDirectoryPath: string = path.join(__dirname, "../public");
 
+app.set('view engine', 'hbs');
+
 app.use(express.static(publicDirectoryPath));
 
-app.get("/", (req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+app.get("", (req: Request, res: Response) => {
+    res.render('index', {
+        title: "Weather app",
+        name: "Sumit Mishra"
+    });
+})
+
+app.get("/about", (req: Request, res: Response) => {
+    res.render('about', {
+        title: "About Me",
+        name: "Sumit Mishra"
+    })
+})
+
+app.get("/help", (req: Request, res: Response) => {
+    res.render('help', {
+        helpTitle: "We are happy to help!"
+    })
 })
 
 app.listen(PORT, () => console.log(`app is running on port ${PORT}`));
